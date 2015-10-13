c6887ebe03b8aa5a1472b0a875fcaa251fb3c33b
Espen Zaal <espenbz@gmail.com>
2015-10-13 17:33:29 +0200

    HAr en fungerende ArrayAdapter, men uten lesbart innhold

--- app/src/main/java/com/example/s198599/s198599_mappe2/fragments/PersonListFragment.java
+++ app/src/main/java/com/example/s198599/s198599_mappe2/fragments/PersonListFragment.java
@@ -1,22 +1,49 @@
 package com.example.s198599.s198599_mappe2.fragments;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.app.ListFragment;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.example.s198599.s198599_mappe2.R;
 import com.example.s198599.s198599_mappe2.dummy.DummyContent;
+import com.example.s198599.s198599_mappe2.models.Person;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 
 public class PersonListFragment extends ListFragment {
 
-    private OnFragmentInteractionListener mListener;
-
+    private ArrayAdapter<Person> adapter;
+    private AppCompatActivity listener;
 
     public PersonListFragment() {
+    }
+
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        ListView lv = (ListView)view.findViewById(R.id.person_list_view);
+
+        lv.setAdapter(adapter);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.person_list_view, container, false);
+
     }
 
     @Override
@@ -24,53 +51,39 @@ public class PersonListFragment extends ListFragment {
         super.onCreate(savedInstanceState);
         Log.d("Birthday", "PersonListFragment - onCreate");
 
-        // TODO: Change Adapter to display your content
-        setListAdapter(new ArrayAdapter<DummyContent.DummyItem>(getActivity(),
-                android.R.layout.simple_list_item_1, android.R.id.text1, DummyContent.ITEMS));
+
+        List<Person> dummyList = new ArrayList<>();
+        dummyList.add(new Person("Espen", "Zaal", "98653942", Calendar.getInstance()));
+        dummyList.add(new Person("Truls", "Pettersen", "90123456", Calendar.getInstance()));
+
+        /*List<String> dummyList = new ArrayList<>();
+        dummyList.add("Test1");
+        dummyList.add("Test2");*/
+
+        adapter = new ArrayAdapter<Person>(getActivity(),
+                android.R.layout.simple_list_item_1, android.R.id.text1, dummyList);
+
+
     }
 
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+        listener = (AppCompatActivity)activity;
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
+        //mListener = null;
     }
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
 
-        if (null != mListener) {
-            // Notify the active callbacks interface (the activity, if the
-            // fragment is attached to one) that an item has been selected.
-            mListener.onFragmentInteraction(DummyContent.ITEMS.get(position).id);
-        }
-    }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(String id);
     }
 
 }
