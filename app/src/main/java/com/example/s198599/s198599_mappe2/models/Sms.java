3728414d14fb2c928c399a20c2cec61a3feefaeb
Espen Zaal <espenbz@gmail.com>
2015-10-17 13:44:03 +0200

    commit :)

--- app/src/main/java/com/example/s198599/s198599_mappe2/models/Sms.java
+++ app/src/main/java/com/example/s198599/s198599_mappe2/models/Sms.java
@@ -1,5 +1,8 @@
 package com.example.s198599.s198599_mappe2.models;
 
+import java.sql.Timestamp;
+import java.util.Calendar;
+
 /**
  * Created by espen on 10/5/15.
  */
@@ -7,9 +10,26 @@ public class Sms {
 
     private int messageId;
     private String message;
+    private Calendar dateTime;
 
-    public Sms(String message) {
+
+    public Sms(String message, Calendar dateTime) {
         this.message = message;
+        this.dateTime = dateTime;
+    }
+
+    public Sms(int messageId, String message, Calendar dateTime) {
+        this.messageId = messageId;
+        this.message = message;
+        this.dateTime = dateTime;
+    }
+
+    public Calendar getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(Calendar dateTime) {
+        this.dateTime = dateTime;
     }
 
     public Sms(){}
